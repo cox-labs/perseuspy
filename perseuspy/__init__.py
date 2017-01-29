3acd866efc81ea3b84fda2849ccf3dc63954e5e4
Jan Rudolph <rudolph@biochem.mpg.de>
2017-01-29 23:42:10 +0100

    numerical annotations; pass kwargs to pandas

--- perseuspy/__init__.py
+++ perseuspy/__init__.py
@@ -47,26 +47,33 @@ def create_column_index(annotations):
     column_index = pd.MultiIndex.from_tuples(list(zip(*_column_index.values())), names=list(_column_index.keys()))
     return column_index
 
-def read_perseus(path_or_file, type_map = perseus_to_dtype):
+def read_perseus(path_or_file, type_map = perseus_to_dtype, **kwargs):
     """
     Read a Perseus-formatted matrix into a pd.DataFrame.
     Annotation rows will be converted into a multi-index.
 
     By monkey-patching the returned pd.DataFrame a `to_perseus`
     method for exporting the pd.DataFrame is made available.
+
+    :param path_or_file: File path or file-like object
+    :param type_map: How to map Perseus types to numpy.dtype
+    :param **kwargs: Keyword arguments passed as-is to pandas.read_csv
+    :returns: The parsed data frame
     """
     annotations = read_annotations(path_or_file, separator, type_map)
     column_index = create_column_index(annotations)
     if 'Type' in annotations:
         dtype = {name : t for name, t in zip(annotations['Column Name'], annotations['Type'])}
-        df = pd.read_csv(path_or_file, sep=separator, comment='#', dtype = dtype)
+        df = pd.read_csv(path_or_file, sep=separator, comment='#', dtype = dtype, **kwargs)
     else:
-        df = pd.read_csv(path_or_file, sep=separator, comment='#')
+        df = pd.read_csv(path_or_file, sep=separator, comment='#', **kwargs)
     df.columns = column_index
     return df
 
 import numpy as np
-def to_perseus(df, path_or_file, main_columns=None, separator=separator, type_map = dtype_to_perseus):
+def to_perseus(df, path_or_file, main_columns=None,
+        separator=separator, type_map = dtype_to_perseus,
+        numerical_annotation_columns = set([])):
     """
     Save pd.DataFrame to Perseus text format.
 
@@ -81,7 +88,8 @@ def to_perseus(df, path_or_file, main_columns=None, separator=separator, type_ma
     annotations['Type'] = ['E' if i in main_columns else type_map[dtype] for i, dtype in enumerate(df.dtypes)]
     annotation_row_names = set(df.columns.names) - {'Column Name'}
     for name in annotation_row_names:
-        annotations['C:{}'.format(name)] = df.columns.get_level_values(name)
+        annotation_type = 'N' if name in numerical_annotation_columns else 'C'
+        annotations['{}:{}'.format(annotation_type, name)] = df.columns.get_level_values(name)
     with PathOrFile(path_or_file, 'w') as f:
         f.write(separator.join(column_names) + '\n')
         for name, values in annotations.items():
