ec676a9cf2757a682835bdf63882837719f6bccb
Jan Rudolph <rudolph@biochem.mpg.de>
2017-03-06 15:39:11 +0100

    add/refactor test

--- perseuspy/test/test_io.py
+++ perseuspy/test/test_io.py
@@ -5,29 +5,39 @@ from perseuspy import pd
 
 TEST_DIR = path.dirname(__file__)
 
+def to_string(df, **kwargs):
+    out = StringIO()
+    df.to_perseus(out, **kwargs)
+    out.seek(0)
+    return '\n'.join(out.readlines())
+
+def type_row(df, **kwargs):
+    lines = to_string(df, **kwargs).splitlines()
+    for line in lines:
+        if line.startswith('#!{Type}'):
+            return line.strip()
+    return ''
+
 class TestReading(TestCase):
     def test_reading_example1(self):
         df = pd.read_perseus(path.join(TEST_DIR, 'matrix.txt'))
         self.assertIsNot(df, None)
-        out = StringIO()
-        df.to_perseus(out)
-        self.assertIsNot(str(out), '')
+        self.assertIsNot(to_string(df), '')
+        self.assertEqual('#!{Type}' + (15 * 'E\t') + 'T', type_row(df))
 
     def test_reading_example2(self):
         df = pd.read_perseus(path.join(TEST_DIR, 'matrix2.txt'))
         self.assertIsNot(df, None)
-        out = StringIO()
-        df.to_perseus(out)
-        self.assertIsNot(str(out), '')
+        self.assertIsNot(to_string(df), '')
+
+    def test_reading_example3(self):
+        df = pd.read_perseus(path.join(TEST_DIR, 'matrix3.txt'))
+        self.assertIsNot(df, None)
+        self.assertIsNot(to_string(df), '')
+        self.assertEqual('#!{Type}' + (3 * 'E\t') + 'C',
+                '\t'.join(type_row(df).split('\t')[:4]))
 
     def test_inferring_and_setting_main_columns(self):
-        def typeRow(df, main_columns=None):
-            out = StringIO()
-            df.to_perseus(out, main_columns)
-            out.seek(0)
-            typeRow = out.readlines()[1]
-            return typeRow
-
         df = pd.DataFrame({'a' : [2,3], 'b': [1,2], 'c': ['a','b'], 'd': [3,4]})
-        self.assertEqual('#!{Type}E\tE\tT\tN\n', typeRow(df))
-        self.assertEqual('#!{Type}N\tE\tT\tE\n', typeRow(df, main_columns={'b','d'}))
+        self.assertEqual('#!{Type}E\tE\tT\tN', type_row(df))
+        self.assertEqual('#!{Type}N\tE\tT\tE', type_row(df, main_columns={'b','d'}))
