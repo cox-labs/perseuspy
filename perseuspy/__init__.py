f8b70fc43b2b403d5d2ea6c5a0bc0f6ec2006745
ldeininger <deininger.luca@gmail.com>
2017-04-20 15:43:03 +0200

    import read/write from perseusNetwork

--- perseuspy/__init__.py
+++ perseuspy/__init__.py
@@ -8,6 +8,7 @@ Currently there is no support for the following Perseus matrix features:
 import perseuspy.dependent_peptides
 import perseuspy.io
 from perseuspy.io.perseus import to_perseus, read_perseus
+from perseuspy.io.perseusNetwork import readNetworks, writeNetworks
 # Monkey-patching pandas
 import pandas as pd
 pd.DataFrame.to_perseus = to_perseus
