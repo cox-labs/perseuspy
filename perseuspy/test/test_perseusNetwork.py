7a8f63ac343dbb4550d1da5b92f709a7f4f7e26c
ldeininger <deininger.luca@gmail.com>
2017-04-21 14:49:50 +0200

    path.join instead of explicit; one more test

--- perseuspy/test/test_perseusNetwork.py
+++ perseuspy/test/test_perseusNetwork.py
@@ -12,12 +12,14 @@ class Tests(unittest.TestCase):
 	"""Tests for perseusNetwork.py"""
 
 	def test_reading(self):
-		allDicts=readNetworks(DIR+"/exampleNetworks/networkFiles")
-		self.assertTrue(not allDicts[0].empty  and any(allDicts[1]) and any(allDicts[2]) and any(allDicts[3]) and any(allDicts[4]))
+		allDicts = readNetworks(os.path.join(DIR, "exampleNetworks", "networkFiles"))
+		self.assertTrue(not allDicts[0].empty  and any(allDicts[1]) and any(allDicts[2]) and any(allDicts[3]) and any(allDicts[4])) # check on empty dictionaries
+		nrNetw = len(allDicts[0].index)
+		self.assertTrue(nrNetw == len(allDicts[1]) and nrNetw == len(allDicts[2]) and nrNetw == len(allDicts[3]) and nrNetw == len(allDicts[1])) # check size of created dictionaries
 		
 	def test_writing(self):
-		outDir = DIR+"/exampleNetworks/outDirectory"
-		dictlist=readNetworks(DIR+"/exampleNetworks/networkFiles")
+		outDir = os.path.join(DIR, "exampleNetworks", "outDirectory")
+		dictlist = readNetworks(os.path.join(DIR, "exampleNetworks", "networkFiles"))
 		writeNetworks(outDir, dictlist)
 		self.assertTrue(len(dictlist[4])*2+1+1 == len([name for name in os.listdir(outDir) if os.path.isfile(os.path.join(outDir, name))])) # check number of created files, +1(networks.txt), +1(dummy file)
 
