ed50813bf3772d1fd66453041861f2a7d1dad0f2
ldeininger <deininger.luca@gmail.com>
2017-04-21 10:27:09 +0200

    changed code order

--- perseuspy/__init__.py
+++ perseuspy/__init__.py
@@ -8,9 +8,9 @@ Currently there is no support for the following Perseus matrix features:
 import perseuspy.dependent_peptides
 import perseuspy.io
 from perseuspy.io.perseus import to_perseus, read_perseus
-from perseuspy.io.perseusNetwork import readNetworks, writeNetworks
 # Monkey-patching pandas
 import pandas as pd
 pd.DataFrame.to_perseus = to_perseus
 pd.read_perseus = read_perseus
+from perseuspy.io.perseusNetwork import readNetworks, writeNetworks
 
