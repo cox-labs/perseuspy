08bf221b36e12395c1a32231abe49c2a852c705a
ldeininger <deininger.luca@gmail.com>
2017-07-31 13:32:00 +0200

    new example networks; minimal rearrangements

--- perseuspy/io/perseusNetwork.py
+++ perseuspy/io/perseusNetwork.py
@@ -3,73 +3,76 @@ import networkx as nx
 from collections import OrderedDict
 from perseuspy import pd # ugly -> change this to internal call
 
-def read_networks(pathInFolder):
-	"""
-	Translating networks and corresponding node-/ and edgeTables to graphs
-	
-	>>> allDicts = read_networks(pathinFolder)
-	
-	:param pathInFolder: Path to directory
-	:returns: List of [networkTable, nodes, edges, nameGUID, graphs]
-	"""
-	networkTable = pd.read_perseus(path.join(pathInFolder, "networks.txt"))
-	nodes = OrderedDict()
-	edges = OrderedDict()
-	nameGUID = OrderedDict()
-	graphs = OrderedDict()
-	for Name, GUID in zip(networkTable.Name, networkTable.GUID):
-		G=nx.DiGraph()
-		nodeTable = pd.read_perseus(path.join(pathInFolder, GUID + "_nodes.txt"))
-		edgeTable = pd.read_perseus(path.join(pathInFolder, GUID + "_edges.txt"))
-		nodes[GUID] = nodeTable
-		edges[GUID] = edgeTable
-		nameGUID[Name] = GUID
-		graphs[GUID] = G
-		for Node in nodeTable.Node:
-			G.add_node(Node)
-		for Source, Target in zip(edgeTable.Source, edgeTable.Target):
-			G.add_edge(Source, Target)
-	allDicts = [networkTable, nodes, edges, nameGUID, graphs]
-	return allDicts
-	
+def read_networks(pathInFolder, directed):
+    """
+    Translating networks and corresponding node-/ and edgeTables to graphs
+    
+    >>> allDicts = read_networks(pathinFolder)
+    
+    :param pathInFolder: Path to directory
+    :returns: List of [networkTable, nodes, edges, nameGUID, graphs]
+    """
+    networkTable = pd.read_perseus(path.join(pathInFolder, "networks.txt"))
+    nodes = OrderedDict()
+    edges = OrderedDict()
+    nameGUID = OrderedDict()
+    graphs = OrderedDict()
+    for Name, GUID in zip(networkTable.Name, networkTable.GUID):
+        if directed == 1:
+            G = nx.DiGraph()
+        else:
+            G = nx.Graph()
+        nodeTable = pd.read_perseus(path.join(pathInFolder, GUID + "_nodes.txt"))
+        edgeTable = pd.read_perseus(path.join(pathInFolder, GUID + "_edges.txt"))
+        nodes[GUID] = nodeTable
+        edges[GUID] = edgeTable
+        nameGUID[Name] = GUID
+        graphs[GUID] = G
+        for Node in nodeTable.Node:
+            G.add_node(Node)
+        for Source, Target in zip(edgeTable.Source, edgeTable.Target):
+            G.add_edge(Source, Target)
+    allDicts = [networkTable, nodes, edges, nameGUID, graphs]
+    return allDicts
+    
 def write_networks(pathOutFolder, allDicts):
-	"""
-	Writing networkTable, nodes and edges to Perseus readable format
-	
-	Important when changing graphs:
-	As 'writeNetworks(...)' currently  writes the networks from networkTable, nodes and edges (not from graphs!),
-	graphs shall represent the same graphs as networkTable, nodes and edges imply.
-	To keep this relation: When changing graph, simultaneously changing networkTables(network).
-	-> For example add_edge(), add_node(), remove_edge(), remove_node() necessary.
-		
-	:param pathOutFolder: Path to directory
-	:param allDicts: List of minimum networkTable, nodes and edges
-	"""
-	_write_table(path.join(pathOutFolder, "networks.txt"), allDicts[0])
-	for key in allDicts[1]:
-		_write_table(path.join(pathOutFolder, key + "_nodes.txt"), allDicts[1][key])
-	for key in allDicts[2]:
-		_write_table(path.join(pathOutFolder, key + "_edges.txt"), allDicts[2][key])
+    """
+    Writing networkTable, nodes and edges to Perseus readable format
+    
+    Important when changing graphs:
+    As 'writeNetworks(...)' currently  writes the networks from networkTable, nodes and edges (not from graphs!),
+    graphs shall represent the same graphs as networkTable, nodes and edges imply.
+    To keep this relation: When changing graph, simultaneously changing networkTables(network).
+    -> For example add_edge(), add_node(), remove_edge(), remove_node() necessary.
+        
+    :param pathOutFolder: Path to directory
+    :param allDicts: List of minimum networkTable, nodes and edges
+    """
+    _write_table(path.join(pathOutFolder, "networks.txt"), allDicts[0])
+    for key in allDicts[1]:
+        _write_table(path.join(pathOutFolder, key + "_nodes.txt"), allDicts[1][key])
+    for key in allDicts[2]:
+        _write_table(path.join(pathOutFolder, key + "_edges.txt"), allDicts[2][key])
 
 def _write_table(path_or_ioobject, table):
-	"""
-	Writing data frame to Perseus readable format. 
-	
-	:param path_or_ioobject: Path for creating file or StringIO object
-	:param table: Pandas data frame
-	"""
-	table.to_perseus(path_or_ioobject)
-			
+    """
+    Writing data frame to Perseus readable format. 
+    
+    :param path_or_ioobject: Path for creating file or StringIO object
+    :param table: Pandas data frame
+    """
+    table.to_perseus(path_or_ioobject)
+            
 def print_network_names(networkTable):
-	"""
-	Printing all network names.
-	
-	:param networkTable: Pandas data frame containing overview of the networks
-	"""
-	print ("\n All Networknames", end=": ")
-	for Name, GUID in zip(networkTable.Name, networkTable.GUID):
-		print (Name, end=", ")
-	print ("\n")
+    """
+    Printing all network names.
+    
+    :param networkTable: Pandas data frame containing overview of the networks
+    """
+    print ("\n All Networknames", end=": ")
+    for Name, GUID in zip(networkTable.Name, networkTable.GUID):
+        print (Name, end=", ")
+    print ("\n")
 
 def add_node(allDicts, nodeName , networkid):
    """
--- perseuspy/test/test_perseusNetwork.py
+++ perseuspy/test/test_perseusNetwork.py
@@ -6,32 +6,34 @@ from io import StringIO
 sys.path.insert(0, path.abspath(path.join(path.dirname( __file__ ), '..', 'io')))
 from perseusNetwork import read_networks, _write_table
 
-inFolder = path.join(path.dirname(__file__), "exampleNetworks", "networkFiles")
+inFolders = [path.join(path.dirname(__file__), "exampleNetwork1", "networkFiles"), path.join(path.dirname(__file__), "exampleNetwork2", "networkFiles"), path.join(path.dirname(__file__), "exampleNetwork3", "networkFiles"), path.join(path.dirname(__file__), "exampleNetwork4", "networkFiles"), path.join(path.dirname(__file__), "exampleNetwork5", "networkFiles")]
 
 class Tests(TestCase):
-	def test_reading(self):
-		allDicts = read_networks(inFolder)
-		self.assertTrue(not allDicts[0].empty and any(allDicts[1]) and any(allDicts[2]) and any(allDicts[3]) and any(allDicts[4]))
-		nrNetw = len(allDicts[0].index)
-		self.assertTrue(nrNetw == len(allDicts[1]) and nrNetw == len(allDicts[2]) and nrNetw == len(allDicts[3]) and nrNetw == len(allDicts[4]))
-		
-	def test_writing(self):
-		allDicts = read_networks(inFolder)
-		networkTable = StringIO()
-		_write_table(networkTable, allDicts[0])
-		nodes_list = []
-		edges_list = []
-		for key in allDicts[1]:
-			nodes = StringIO()
-			_write_table(nodes, allDicts[1][key])
-			nodes_list.append(nodes)
-		for key in allDicts[2]:
-			edges = StringIO()
-			_write_table(edges, allDicts[2][key])
-			edges_list.append(edges)
-		self.assertTrue(len(nodes_list) == len(edges_list) and len(edges_list) == allDicts[0].shape[0] and len(nodes_list) != 0)
-		for tableio in nodes_list+edges_list:
-			self.assertTrue(tableio.getvalue() != "")
+    def test_reading(self):
+        for netw in inFolders:
+            allDicts = read_networks(netw, 1)
+            self.assertTrue(not allDicts[0].empty and any(allDicts[1]) and any(allDicts[2]) and any(allDicts[3]) and any(allDicts[4]))
+            nrNetw = len(allDicts[0].index)
+            self.assertTrue(nrNetw == len(allDicts[1]) and nrNetw == len(allDicts[2]) and nrNetw == len(allDicts[3]) and nrNetw == len(allDicts[4]))
+        
+    def test_writing(self):
+        for netw in inFolders:
+            allDicts = read_networks(netw, 1)
+            networkTable = StringIO()
+            _write_table(networkTable, allDicts[0])
+            nodes_list = []
+            edges_list = []
+            for key in allDicts[1]:
+                nodes = StringIO()
+                _write_table(nodes, allDicts[1][key])
+                nodes_list.append(nodes)
+            for key in allDicts[2]:
+                edges = StringIO()
+                _write_table(edges, allDicts[2][key])
+                edges_list.append(edges)
+            self.assertTrue(len(nodes_list) == len(edges_list) and len(edges_list) == allDicts[0].shape[0] and len(nodes_list) != 0)
+            for tableio in nodes_list+edges_list:
+                self.assertTrue(tableio.getvalue() != "")
 
 if __name__ == '__main__':
     main()
