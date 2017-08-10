529011c3d601a80156a22d2fe8e54b0d20cc2fe4
Jan Rudolph <rudolph@biochem.mpg.de>
2017-08-10 11:01:39 +0200

    fix installation without pandas

--- perseuspy/__init__.py
+++ perseuspy/__init__.py
@@ -5,12 +5,14 @@ Currently there is no support for the following Perseus matrix features:
  - numerical annotation rows
  - multi-numeric rows
 """
-import perseuspy.dependent_peptides
-import perseuspy.io
-from perseuspy.io.perseus import to_perseus, read_perseus
-# Monkey-patching pandas
-import pandas as pd
-pd.DataFrame.to_perseus = to_perseus
-pd.read_perseus = read_perseus
-
-__version__ = '0.1.2'
+__version__ = '0.1.3'
+try:
+    import perseuspy.dependent_peptides
+    import perseuspy.io
+    from perseuspy.io.perseus import to_perseus, read_perseus
+    # Monkey-patching pandas
+    import pandas as pd
+    pd.DataFrame.to_perseus = to_perseus
+    pd.read_perseus = read_perseus
+except ImportError as e: # this shold happen only during installation
+    print(e)
